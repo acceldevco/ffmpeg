801f1eece8c36a8dc52aa0b635391c44710beee8
acceldevco <166697421+acceldevco@users.noreply.github.com>
2025-02-24 15:05:33 +0330

    Update route.ts

--- app/api/edit-video/route.ts
+++ app/api/edit-video/route.ts
@@ -71,9 +71,9 @@ export async function POST(req: NextRequest) {
   }
 }
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// }
 
